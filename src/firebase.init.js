9b66288e90ef85a04f7c426a05cd67235f3dcc1e
MD RAIHANUL KABIR <rkbokul@gmail.com>
2022-04-25 20:06:10 +0600

    enviroment variable done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,19 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-   apiKey: "AIzaSyCsaiyXXHD2K5LwIAC3OqR7ONref269ATI",
-  authDomain: "independent-service-prov-a311f.firebaseapp.com",
-  projectId: "independent-service-prov-a311f",
-  storageBucket: "independent-service-prov-a311f.appspot.com",
-  messagingSenderId: "900742211889",
-  appId: "1:900742211889:web:28fc043066a97958326810"
+apiKey: process.env.REACT_APP_apiKey,
+authDomain: process.env.REACT_APP_authDomain,
+projectId: process.env.REACT_APP_projectId,
+storageBucket: process.env.REACT_APP_storageBucket,
+messagingSenderId: process.env.REACT_APP_messagingSenderId,
+appId: process.env.REACT_APP_appId,
+
+  // apiKey: "AIzaSyCsaiyXXHD2K5LwIAC3OqR7ONref269ATI",
+  // authDomain: "independent-service-prov-a311f.firebaseapp.com",
+  // projectId: "independent-service-prov-a311f",
+  // storageBucket: "independent-service-prov-a311f.appspot.com",
+  // messagingSenderId: "900742211889",
+  // appId: "1:900742211889:web:28fc043066a97958326810"
 };
 
 // Initialize Firebase
